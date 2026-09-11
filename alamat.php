66029f53a09eaa6e8e5e86874af0cbccc6d0e52d
arkanaganteng2008 <arkanaalfarizidevantara@gmail.com>
2026-09-11 08:03:16 +0700

    Add files via upload

---
+++ alamat.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<style>
+.opening{
+    background-color: rgb(30, 82, 253);
+    padding:15px;
+    margin:30px;
+    border-radius: 30px;
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: 20px;
+}
+</style>
+
+
+<body>
+    <div class="opening">
+        <h1><center>Welcome To My Web Personal</center></h1>
+    </div>
+
+    <div class="container text-center">
+  <div class="row row-cols-5">
+    
+      <a href="index.php" class="btn btn-outline-primary">Home</a>
+    
+
+    
+      <a href="biodata.php" class="btn btn-outline-primary">Biodata</a>
+    
+
+    
+      <a href="aboutme.php" class="btn btn-outline-primary">About Me</a>
+    
+
+    
+      <a href="hobi.php" class="btn btn-outline-primary">Hobby</a>
+    
+
+    
+      <a href="pengalaman.php" class="btn btn-outline-primary">Pengalaman</a>
+    
+    
+  </div>
+</div>
+
+
+    
+
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<style>
+.opening{
+    background-color: rgb(30, 82, 253);
+    padding:15px;
+    margin:30px;
+    border-radius: 30px;
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: 20px;
+}
+
+body{
+    background-color: aqua;
+}
+</style>
+
+<body>
+    <div class="opening">
+        <h1><center>Welcome To My Web Personal</center></h1>
+    </div>
+
+<div class="container text-center">
+  <div class="row row-cols-5">
+    
+      <a href="index.php" class="btn btn-outline-primary">Home</a>
+    
+
+    
+      <a href="biodata.php" class="btn btn-outline-primary">Biodata</a>
+    
+
+    
+      <a href="aboutme.php" class="btn btn-outline-primary">About Me</a>
+    
+
+    
+      <a href="hobi.php" class="btn btn-outline-primary">Hobby</a>
+    
+
+    
+      <a href="pengalaman.php" class="btn btn-outline-primary">Pengalaman</a>
+    
+    
+  </div>
+</div>
+
+
+
+
+    <div class="card" style="width: 18rem; float:left;">
+      <img src="matikanefukukitaru_icon.png" class="card-img-top" alt="...">
+         <div class="card-body">
+             <h5 class="card-title">Sekilas TentangKu</h5>
+             <p class="card-text"> Aku Adalah Seorang Programmer, Ingin Tau Lebih Lanjut Klik Tombol Dibawah</p>
+             <a href="biodata.php" class="btn btn-primary">Biodata</a>
+         </div>
+    </div>
+
+
+    <div class="card" style="float:left;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content. blablablablablaa</p>
+  </div>
+</div>
+
+    
+</body>
+</html>
